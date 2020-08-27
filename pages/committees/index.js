0f91f1974d7c727a6295e381fef5b32b33367598
Nishant Shah <nishant.shah@iga.in.gov>
2020-08-27 19:31:56 -0400

    Clean up CSS, add a global CSS file, add a Committees directory and index.js file

--- pages/committees/index.js
+++ pages/committees/index.js
@@ -1,10 +1,14 @@
-import Layout from "../components/Layout";
 import React from "react";
+import Head from "next/head";
+import Layout from "../../components/Layout";
 
 const Committees = () => (
   <Layout>
+    <Head>
+      <title>Committees</title>
+    </Head>
     <div className="container-fluid content h-100">
-      <h1>Committees Homepage (List view)</h1>
+      <h1>Committees Homepage Index (List view)</h1>
     </div>
 
     <style jsx>{`
