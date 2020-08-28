0ac6d408ae974e6c0151f6cf04cbeb17c705a22a
henok.alem <henok.alem@iga.in.gov>
2020-08-28 12:51:30 -0400

    Big dropdown

--- pages/committees/index.js
+++ pages/committees/index.js
@@ -3,7 +3,6 @@ import Link from "next/link";
 
 import CommitteeLayout from "../../components/Committees/CommitteeLayout";
 import * as committees from "../../data/committees.json";
-import * as conference_committees from "../../data/conference_committees";
 
 const Committees = ({committees}) => (
   <CommitteeLayout>
