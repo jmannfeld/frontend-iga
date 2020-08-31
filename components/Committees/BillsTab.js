a56ec60c8dcf213b36b8f374e3c939f501eee121
Jordan Mannfeld <jordan.mannfeld@iga.in.gov>
2020-08-31 18:52:56 -0400

    adding tabs to commitee details page

---
+++ components/Committees/BillsTab.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const BillsTab = ({ committeeLpid }) => (
+  <>
+    <div>bills tab</div>
+    <style jsx>{`
+      .content {
+        background-color: white;
+        border: 2px solid red;
+      }
+    `}</style>
+  </>
+);
+
+export default BillsTab;
--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,35 @@
-import React from "react";
+import React from 'react';
 
 import '../styles/global.css';
-import Layout from "../components/Layout";
 
-import * as standing_committees from "../data/standing_committees.json";
-import * as interim_committees from "../data/interim_committees.json";
+import Layout from '../components/Layout';
 
-const App = ({Component, pageProps}) => (
-  <Layout standing_committees={standing_committees} interim_committees={interim_committees}>
-    <Component {...pageProps}/>
+import * as standing_committees from '../data/standing_committees.json';
+import * as interim_committees from '../data/interim_committees.json';
+
+const App = ({ Component, pageProps }) => (
+  <Layout
+    standing_committees={standing_committees}
+    interim_committees={interim_committees}
+  >
+    <Component {...pageProps} />
   </Layout>
 );
 
 export async function getStaticProps() {
-  const standing_committees_response = JSON.parse(JSON.stringify(standing_committees));
-  const interim_committees_response = JSON.parse(JSON.stringify(interim_committees));
+  const standing_committees_response = JSON.parse(
+    JSON.stringify(standing_committees)
+  );
+  const interim_committees_response = JSON.parse(
+    JSON.stringify(interim_committees)
+  );
   this.standing_committees = standing_committees_response.default;
   return {
     props: {
       standing_committees: standing_committees_response.default,
-      interim_committees: interim_committees_response.default
-    }
-  }
+      interim_committees: interim_committees_response.default,
+    },
+  };
 }
 
 export default App;
