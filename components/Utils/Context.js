2b83de5243b12669a4f492c6d71b9a8cd0bff652
Nishant Shah <nishant.shah@iga.in.gov>
2020-09-01 12:06:29 -0400

    Update context fetch data

--- components/Utils/Context.js
+++ components/Utils/Context.js
@@ -5,7 +5,29 @@ export const Context = createContext(null);
 export class Provider extends React.Component {
   constructor(props) {
     super(props);
-    this.state = props.state;
+    this.state = {
+      senate_standing_committees: [],
+      house_standing_committees: [],
+      interim_legislative_committees: [],
+      interim_other_committees: [],
+      conference_committees: []
+    };
+    fetch("http://localhost:3004/committees")
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        this.setState({
+          senate_standing_committees: data.filter(
+            committee => committee.type === "standing" && committee.chamber === "senate"
+          ),
+          house_standing_committees: data.filter(
+            committee => committee.type === "standing" && committee.chamber === "house"
+          ),
+          interim_legislative_committees: data.filter(committee => committee.type === "interim" && committee.non_lsa_staffed === false),
+          interim_other_committees: data.filter(committee => committee.type === "interim" && committee.non_lsa_staffed === true),
+          conference_committees: data.filter(committee => committee.type === "conference")
+        });
+      });
   }
 
   render() {
--- pages/_app.js
+++ pages/_app.js
@@ -6,29 +6,12 @@ import '../styles/global.scss';
 
 import Layout from '../components/Layout';
 import {Provider} from "../components/Utils/Context";
-import * as committeesList from "../data/committees";
 
 class MyApp extends App {
-  constructor(props) {
-    super(props);
-
-    const response = JSON.parse(JSON.stringify(committeesList)); // this is where we would have the API call, e.g. await fetch(...)
-    this.state = {
-      senate_standing_committees: response.default.committees.filter(
-        committee => committee.type === "standing" && committee.chamber === "senate"
-      ),
-      house_standing_committees: response.default.committees.filter(
-        committee => committee.type === "standing" && committee.chamber === "house"
-      ),
-      interim_committees: response.default.committees.filter(committee => committee.type === "interim"),
-      conference_committees: response.default.committees.filter(committee => committee.type === "conference")
-    };
-  }
-
   render() {
     const {Component, pageProps} = this.props;
     return (
-      <Provider state={this.state}>
+      <Provider>
         <Layout>
           <Component {...pageProps} />
         </Layout>
