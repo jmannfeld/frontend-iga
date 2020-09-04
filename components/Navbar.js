437ff0e1ae5e9e9b11caf152da6998b87503a2f6
Nishant Shah <nishant.shah@iga.in.gov>
2020-09-03 21:05:26 -0400

    Fix the Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -115,87 +115,86 @@ const Navbar = () => (
     <li className="nav-item dropdown">
       <Link href="/committees"><a className="nav-link active" role="button">Committees</a></Link>
       <div className="dropdown-menu nav-display">
-
-          <div className="standing-committees">
-            <Link href="/committees/standing"><a className="dropdown-item">Standing</a></Link>
-              <div id="standing-menu" className="sub-dropdown dropdown-menu committee-tables">
-                  <table className="table table-striped house-committees">
-                    <thead className="thead-light">
-                      <tr><th scope="col"><h6>House</h6></th></tr>
-                    </thead>
-                    <Context.Consumer>
-                      {(context) => (
-                        context.state.house_standing_committees.map(({ lpid, name}) => (
-                          <tr className="list-group-item-action">
-                            <td className="nav-item" key={lpid}>
-                              <Link href="/committees/[lpid]" as={`/committees/${lpid}`}>
-                                <a className="dropdown-item text-left">{name}</a>
-                              </Link>
-                            </td>
-                          </tr>
-                        ))
-                      )}
-                    </Context.Consumer>
-                  </table>
-                  <table className="table table-striped senate-committees">
-                    <thead className="thead-light">
-                      <tr><th scope="col"><h6>Senate</h6></th></tr>
-                    </thead>
-                    <Context.Consumer>
-                      {(context) => (
-                        context.state.senate_standing_committees.map(({ lpid, name}) => (
-                          <tr className="list-group-item-action">
-                            <td className="nav-item" key={lpid}>
-                              <Link href="/committees/[lpid]" as={`/committees/${lpid}`}>
-                                <a className="dropdown-item text-left">{name}</a>
-                              </Link>
-                            </td>
-                          </tr>
-                        ))
-                      )}
-                    </Context.Consumer>
-                  </table>
-              </div>
-          </div>
-          <div className="interim-committees">
-            <Link href="/committees/interim"><a className="dropdown-item">Interim</a></Link>
-              <ul id="interim-menu" className="sub-dropdown dropdown-menu list-group">
-                <Context.Consumer>
-                  {(context) => (
-                    context.state.interim_legislative_committees.map(({lpid, name}) => (
-                      <li className="nav-item list-group-item list-group-item-action" key={lpid}>
+        <div className="standing-committees">
+          <Link href="/committees/standing"><a className="dropdown-item">Standing</a></Link>
+          <div id="standing-menu" className="sub-dropdown dropdown-menu committee-tables">
+            <table className="table table-striped house-committees">
+              <thead className="thead-light">
+                <tr><th scope="col"><h6>House</h6></th></tr>
+              </thead>
+              <Context.Consumer>
+                {(context) => (
+                  context.state.house_standing_committees.map(({ lpid, name}) => (
+                    <tr className="list-group-item-action">
+                      <td className="nav-item" key={lpid}>
                         <Link href="/committees/[lpid]" as={`/committees/${lpid}`}>
                           <a className="dropdown-item text-left">{name}</a>
                         </Link>
-                      </li>
-                    ))
-                  )}
-                </Context.Consumer>
-              </ul>
+                      </td>
+                    </tr>
+                  ))
+                )}
+              </Context.Consumer>
+            </table>
+            <table className="table table-striped senate-committees">
+              <thead className="thead-light">
+                <tr><th scope="col"><h6>Senate</h6></th></tr>
+              </thead>
+              <Context.Consumer>
+                {(context) => (
+                  context.state.senate_standing_committees.map(({ lpid, name}) => (
+                    <tr className="list-group-item-action">
+                      <td className="nav-item" key={lpid}>
+                        <Link href="/committees/[lpid]" as={`/committees/${lpid}`}>
+                          <a className="dropdown-item text-left">{name}</a>
+                        </Link>
+                      </td>
+                    </tr>
+                  ))
+                )}
+              </Context.Consumer>
+            </table>
           </div>
-          <div className="conference-committees">
-            <Link href="/committees/conference"><a className="dropdown-item">Conference</a></Link>
-          </div>
-          <li>
-            <Link href="#">
-              <a className="dropdown-item misc">House Committee Schedule <FaRegFilePdf /></a>
-            </Link>
-          </li>
-          <li>
-            <Link href="#">
-              <a className="dropdown-item misc">Senate Committee Schedule <FaRegFilePdf /></a>
-            </Link>
-          </li>
-          <li>
-            <Link href="#">
-              <a className="dropdown-item misc">House Conference Committee Schedule <FaRegFilePdf /></a>
-            </Link>
-          </li>
-          <li>
-            <Link href="#">
-              <a className="dropdown-item misc">Senate Conference Committee Schedule <FaRegFilePdf /></a>
-            </Link>
-          </li>
+        </div>
+        <div className="interim-committees">
+          <Link href="/committees/interim"><a className="dropdown-item">Interim</a></Link>
+            <ul id="interim-menu" className="sub-dropdown dropdown-menu list-group">
+              <Context.Consumer>
+                {(context) => (
+                  context.state.interim_legislative_committees.map(({lpid, name}) => (
+                    <li className="nav-item list-group-item list-group-item-action" key={lpid}>
+                      <Link href="/committees/[lpid]" as={`/committees/${lpid}`}>
+                        <a className="dropdown-item text-left">{name}</a>
+                      </Link>
+                    </li>
+                  ))
+                )}
+              </Context.Consumer>
+            </ul>
+        </div>
+        <div className="conference-committees">
+          <Link href="/committees/conference"><a className="dropdown-item">Conference</a></Link>
+        </div>
+        <div>
+          <Link href="#">
+            <a className="dropdown-item">House Committee Schedule <FaRegFilePdf /></a>
+          </Link>
+        </div>
+        <div>
+          <Link href="#">
+            <a className="dropdown-item">Senate Committee Schedule <FaRegFilePdf /></a>
+          </Link>
+        </div>
+        <div>
+          <Link href="#">
+            <a className="dropdown-item">House Conference Committee Schedule <FaRegFilePdf /></a>
+          </Link>
+        </div>
+        <div>
+          <Link href="#">
+            <a className="dropdown-item">Senate Conference Committee Schedule <FaRegFilePdf /></a>
+          </Link>
+        </div>
       </div>
     </li>
     <li className="nav-item dropdown">
